0835108219e2667a371d86b670db48b73d04209b
kentuki18 <34239581+kentuki18@users.noreply.github.com>
2026-05-15 18:06:03 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import Application, CommandHandler, ContextTypes
 
 TOKEN = "8922234866:AAHqGuQa1St_jP0BQ3ZpA-4F36nRLCs9uq4"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("🔮 Arcana AI Bot работает!")
 
-app = ApplicationBuilder().token(TOKEN).build()
+app = Application.builder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
 
