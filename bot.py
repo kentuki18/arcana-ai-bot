33cae267e008a7e5a92df13654c9e9f0951de8c8
kentuki18 <34239581+kentuki18@users.noreply.github.com>
2026-05-17 08:05:02 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,4 +36,5 @@ app = Application.builder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
+print("Bot is running...")
 app.run_polling()
